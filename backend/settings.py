df4fd844ca39c942a9cc5fd1cca1956c6a9d714c
farneet24 <farneetsingh.abhi@gmail.com>
2023-09-20 00:57:56 +0530

    cors

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4lb%$z5xrkuj$v7pzs25l8@jz2lb6dkl4)@_*_c^vkg87rh^en
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1' ]
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
