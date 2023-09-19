23aa83b7068fb0698052630e43f153d3b2998a83
farneet24 <farneetsingh.abhi@gmail.com>
2023-09-19 23:40:19 +0530

    Hello

--- backend/settings.py
+++ backend/settings.py
@@ -55,6 +55,10 @@ MIDDLEWARE = [
     'backhome.ratelimit_middleware.SimpleRateLimitMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'backend.urls'
